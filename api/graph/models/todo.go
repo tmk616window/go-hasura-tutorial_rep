885643a1ce94ed7d30979591e601971338de7a62
難波 智貴 <1113053@PC1212.local>
2022-06-27 14:30:42 +0900

    todoLabelResolver追加

---
+++ api/graph/models/todo.go
@@ -0,0 +1,11 @@
+package models
+
+type Todo struct {
+	ID          string  `json:"id"`
+	Title       string  `json:"title"`
+	Description *string `json:"description"`
+	UserID      int     `json:"userID"`
+	StatusID    int     `json:"statusID"`
+	PriorityID  int     `json:"priorityID"`
+	FinishedAt  string  `json:"finished_at"`
+}
