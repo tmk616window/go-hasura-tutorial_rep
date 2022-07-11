9fd9eff6675a502ff2a869068a12251e11fa4853
難波 智貴 <1113053@PC1212.local>
2022-07-11 17:49:47 +0900

    cloudbuild　修正

--- api/postgresql/db.go
+++ api/postgresql/db.go
@@ -13,7 +13,7 @@ func DBConnect() *gorm.DB {
 	if err != nil {
 		fmt.Printf("読み込み出来ませんでした: %v", err)
 	}
-	dsn := fmt.Sprintf("host=10.125.208.7 user=user password=password  port=5432 database=db")
+	dsn := fmt.Sprintf("host=10.125.208.7 user=user password=password port=5432 database=db")
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		fmt.Printf(err.Error())
